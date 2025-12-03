62962a98ac5d5a88cc2428b6dad00be2a571924d
gm3 <tech.eng.me@gmail.com>
2025-12-03 12:43:26 +1000

    remove alchemy rpc

--- src/viem/getPublicClient.ts
+++ src/viem/getPublicClient.ts
@@ -1,12 +1,10 @@
 import { createPublicClient, http, PublicClient } from 'viem';
 import getViemNetwork from '../viem/getViemNetwork';
-import getAlchemyRpcUrl from '../alchemy/getAlchemyRpcUrl';
 
 export const getPublicClient = (chainId: number) => {
   const chain = getViemNetwork(chainId) as any;
-  const RPC_URL = getAlchemyRpcUrl(chainId);
   return createPublicClient({
     chain,
-    transport: http(RPC_URL),
+    transport: http(),
   }) as PublicClient;
 };
