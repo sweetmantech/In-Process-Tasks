2813920177c7d84bc1d4a8b298ed2f60aa549152
k.c <tech.eng.me@gmail.com>
2026-01-28 22:41:13 +1000

    remove mms trigger

--- src/mms/getApiEndpointUrl.ts
+++
@@ -1,9 +0,0 @@
-import { baseSepolia } from 'viem/chains';
-
-const getApiEndpointUrl = (chainId?: number): string => {
-  return chainId === baseSepolia.id
-    ? 'https://in-process-git-test-sweetmantechs-projects.vercel.app'
-    : 'https://inprocess.world';
-};
-
-export default getApiEndpointUrl;
