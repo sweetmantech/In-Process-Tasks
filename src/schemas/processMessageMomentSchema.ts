e5cf5ebe9991f32bf3bb89b1329f03dafc89d126
k.c <tech.eng.me@gmail.com>
2026-02-01 02:13:50 +1000

    fix zod schema

--- src/schemas/processMessageMomentSchema.ts
+++ src/schemas/processMessageMomentSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const processMessageMomentSchema = z.object({
-  messageId: z.string(),
+  messageId: z.string().uuid(),
 });
 
 export type ProcessMessageMomentPayload = z.infer<
