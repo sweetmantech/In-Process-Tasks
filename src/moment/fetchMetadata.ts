0d5c524beeb1b62f2540253f8eaf9165c691ca5e
ziad <tech.eng.me@gmail.com>
2026-03-11 11:06:39 +1000

    add retry logic

--- src/moment/fetchMetadata.ts
+++ src/moment/fetchMetadata.ts
@@ -4,7 +4,32 @@ import { TokenMetadataJson } from '../ipfs/types';
 const fetchMetadata = async (uri: string): Promise<TokenMetadataJson> => {
   const response = await retry.fetch(
     `https://api.inprocess.world/api/metadata?uri=${encodeURIComponent(uri)}`,
-    { timeoutInMs: 30_000 }
+    {
+      timeoutInMs: 30_000,
+      retry: {
+        timeout: {
+          maxAttempts: 5,
+          minTimeoutInMs: 1_000,
+          maxTimeoutInMs: 30_000,
+          factor: 2,
+        },
+        connectionError: {
+          maxAttempts: 5,
+          minTimeoutInMs: 1_000,
+          maxTimeoutInMs: 30_000,
+          factor: 2,
+        },
+        byStatus: {
+          '400,408,429,5xx': {
+            strategy: 'backoff',
+            maxAttempts: 5,
+            minTimeoutInMs: 1_000,
+            maxTimeoutInMs: 30_000,
+            factor: 2,
+          },
+        },
+      },
+    }
   );
 
   if (!response.ok) {
