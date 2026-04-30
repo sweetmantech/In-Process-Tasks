988314716448c9058767b301dea7626efa63478a
ziad <tech.eng.me@gmail.com>
2026-05-01 04:57:34 +1000

    fix feedback

--- src/schemas/processMessageMomentSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const processMessageMomentSchema = z.object({
-  messageId: z.string().uuid(),
-});
-
-export type ProcessMessageMomentPayload = z.infer<
-  typeof processMessageMomentSchema
->;
